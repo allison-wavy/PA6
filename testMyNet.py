87894e52bcd1e829cc272c2b74cbce8f6c7b203d
Clint Lawson <lawson.528@osu.edu>
2020-11-10 05:23:41 -0500

    Fixed training, broke testing

--- testMyNet.py
+++ testMyNet.py
@@ -15,37 +15,50 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(2, 2)
 
     def forward(self, x):
-        x = self.fc1(x)
         x = F.relu(self.fc1(x))
+        x = self.fc1(x)
         return x
 
-    def train(self, net, labels, fileName):
-        epochs = 10
-        for e in range(0, epochs):
-            with open(fileName, "r") as file:
-                for line in file:
-                    npArr = np.fromstring(line, dtype=float, sep="")
-
-                    classNum = np.delete(npArr, 0, None)
-                    classNum = np.delete(classNum, 0, None)
-
-                    npArrNoClass = np.delete(npArr, 2, None)
-
-                    data = torch.from_numpy(npArrNoClass).type(torch.FloatTensor)
-
-                    out = net(data)
-                    print(out)
-
+    def test(self, net, labels, fileName):
+        epochs = 20
         correct = 0
         total = 0
 
-        print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+        net.load_state_dict(torch.load('./myNet.pth'))
+
+        with open(fileName, "r") as file:
+            npArr = np.genfromtxt(file, dtype=float, delimiter=' ')
+            npArrNoClass = np.delete(npArr, 2, 1)
+            data = torch.from_numpy(npArrNoClass).type(torch.FloatTensor)
+            F.normalize(data, dim=0)
+            #print(data)
+
+        for e in range(0, epochs):
+            total += 3
+            out = net(data)
+            print(out) #there should be epochs * 3 number of rows total among all tensors outputed
+        
+        #TODO: out and prediction are both the same every iteration right now
+            for row in range(3):
+                _, predicted = torch.max(out.data[row], 0)
+                print(predicted)
+                correct += (predicted == labels[row]).sum().item()
+
+
+        #TODO: somehow need to include the mean and standard deviation as calculated via training
+        #Also, need to change how correct things are found.
+        
+        print('Total: %d, Correct: %d' % (total, correct))
+            
+        print('Testing done. Accuracy: %d %%' % (100 * correct / total))
 
 
 def main():
-    fileName = sys.argv[1]
+    labels = torch.tensor([[-1.0, 1.0], [-1.0, 1.0], [-1.0, 1.0]])
     net = Net()
-    model = torch.load('./myNet.pth')
+
+    fileName = sys.argv[1]
+    net.test(net, labels, fileName)
 
 
 if __name__ == '__main__':
--- trainMyNet.py
+++ trainMyNet.py
@@ -15,67 +15,58 @@ import sys
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        # layer one has 2 inputs and 2 outputs
         self.fc1 = nn.Linear(2, 2)
 
     def forward(self, x):
-        x = self.fc1(x)
         x = F.relu(self.fc1(x))
-        return x
+        x = self.fc1(x)
+        return x        
 
     def train(self, net, labels, fileName):
-        epochs = 10
+        epochs = 20
+        criterionMSE = nn.MSELoss()
+        optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0) 
+
+        with open(fileName, "r") as file:
+            npArr = np.genfromtxt(file, dtype=float, delimiter=' ')
+            npArrNoClass = np.delete(npArr, 2, 1)
+            data = torch.from_numpy(npArrNoClass).type(torch.FloatTensor)
+            F.normalize(data, dim=0)
+
         for e in range(0, epochs):
             running_loss = 0.0
-            # read a line from the file
-            with open(fileName, "r") as file:
-                for line in file:
-                    # create a numpy array of floats from the line using space as separator
-                    npArr = np.fromstring(line, dtype=float, sep=" ")
-                    # get the class number from the 3rd column to compare against outputline
-                    classNum = np.delete(npArr, 0, None)
-                    classNum = np.delete(classNum, 0, None)
-                    # remove the class number from the data, only use it to compare output
-                    npArrNoClass = np.delete(npArr, 2, None)
-                
-                    # print(npArrNoClass)
-                    # convert that data numpy array into a float tensor using torch
-                    data = torch.from_numpy(npArrNoClass).type(torch.FloatTensor)
-                    # print(e, data)  
-    
-                    # measure mean squared loss
-                    criterionMSE = nn.MSELoss() # move outside 
-                    optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0) # move outside
-                    optimizer.zero_grad() 
+                    
+            optimizer.zero_grad() 
 
-                    out = net(data)
-                    print(out)
-                    loss = criterionMSE(out, labels)
-                    # calculate the backward gradients for back propagation 
-                    loss.backward()
-                    # update parameters 
-                    optimizer.step()
+            out = net(data)
+            #print(out)
 
-                    # found the running_loss stuff in a pytorch example here: https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html#sphx-glr-beginner-blitz-cifar10-tutorial-py
-                    running_loss += loss.item()
-                    # print('[%d, %5d] loss: %.3f' % (epochs + 1, e + 1, running_loss))
+            loss = criterionMSE(out, labels)
+            # calculate the backward gradients for back propagation 
+            loss.backward()
+            #print(loss)
 
+            # found the running_loss stuff in a pytorch example here: https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html#sphx-glr-beginner-blitz-cifar10-tutorial-py
+            running_loss += loss.item()
+            #print('[Epoch %d] loss: %.3f' % (e + 1, running_loss))
 
-                    # found the two lines below at https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html#sphx-glr-beginner-blitz-cifar10-tutorial-py
-                    PATH = './myNet.pth'
-                    torch.save(net.state_dict(), PATH) # move outside
+            # update parameters 
+            optimizer.step()
+
+        #TODO: We still need to calculate the mean and standard deviation for use in testing
+        #according to the assignment instructions
+
+        # found the two lines below at https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html#sphx-glr-beginner-blitz-cifar10-tutorial-py
+        PATH = './myNet.pth'
+        torch.save(net.state_dict(), PATH) 
+
+        print('Training completed, network saved to myNet.pth')
 
 def main():
-    labels = torch.tensor([1.0, -1.0])
+    labels = torch.tensor([[-1.0, 1.0], [-1.0, 1.0], [-1.0, 1.0]])
     net = Net()
     fileName = sys.argv[1]
     net.train(net, labels, fileName)
-    #net.test(fileName, trainingSet)
 
 if __name__ == "__main__":
     main()
-
-# Questions I still have: 
-    # 1. Are we falling forward anywhere?  Do we need to?
-    # 2. In one of the lectures where he showed us how to define a NN, he created a network input tensor like this: x = torch.tensor([0.3, 0.7, -0.1])
-        # then he declared labels in a separate tensor.  So do we need to declare an x? Or is that we're doing in lines 40-50?
